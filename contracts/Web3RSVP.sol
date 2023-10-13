0808f9e0c806a87bd8c94765475944253bf4012c
rabobahago <rabobahago@gmail.com>
2023-10-13 14:46:07 +0100

    createNewEvent function added

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -4,16 +4,47 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Web3RSVP {
-     struct CreateEvent{
+    struct CreateEvent {
         bytes32 eventId;
         string eventDataCID;
         address eventOwner;
-       uint256 eventTimestamp;
-       uint256 deposit;
-       uint256 maxCapacity;
-       address[] confirmedRSVPs;
-       address[] claimedRSVPs;
-       bool paidOut;
-     }
-     mapping (bytes32 => CreateEvent) idToEvent;
+        uint256 eventTimestamp;
+        uint256 deposit;
+        uint256 maxCapacity;
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs;
+        bool paidOut;
+    }
+    mapping(bytes32 => CreateEvent) idToEvent;
+
+    function createNewEvent(
+        uint256 _eventTimestamp,
+        uint256 _deposit,
+        uint256 _maxCapacity,
+        string calldata _eventDataCID
+    ) external {
+        bytes32 eventId = keccak256(
+            abi.encodePacked(
+                msg.sender,
+                address(this),
+                _eventTimestamp,
+                _deposit,
+                _maxCapacity
+            )
+        );
+
+        address[] memory confirmedRSVPs;
+        address[] memory claimedRSVPs;
+        idToEvent[eventId] = CreateEvent(
+            eventId,
+            _eventDataCID,
+            msg.sender,
+            _eventTimestamp,
+            _deposit,
+            _maxCapacity,
+            confirmedRSVPs,
+            claimedRSVPs,
+            false
+        );
+    }
 }
