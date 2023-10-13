88471d20c489f9d7aeda8d7c5666a3308bfe8ac6
rabobahago <rabobahago@gmail.com>
2023-10-13 14:12:14 +0100

    Web3RSVP contract struct implemented

---
+++ contracts/Web3RSVP.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Web3RSVP {
+     struct CreateEvent{
+        bytes32 eventId;
+        string eventDataCID;
+        address eventOwner;
+       uint256 eventTimestamp;
+       uint256 deposit;
+       uint256 maxCapacity;
+       address[] confirmedRSVPs;
+       address[] claimedRSVPs;
+       bool paidOut;
+     }
+}
